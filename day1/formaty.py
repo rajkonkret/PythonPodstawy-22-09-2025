a48eae060007af06f55e706664271c7b467ddb4a
rajkonkret <rajkonkret660@gmail.com>
2025-09-22 14:16:52 +0200

    [1 15] update formaty.py

--- day1/formaty.py
+++ day1/formaty.py
@@ -19,4 +19,23 @@ print("Witaj %(user)a, jesteś %(user)a" % {"user": user})
 
 print(f"Witaj {user}, masz teraz {wiek} lat.")  # Witaj Tomek, masz teraz 39 lat.
 
-print('')
+print('Używamy wersji pythona %i' % 3)  # Używamy wersji pythona 3
+print('Używamy wersji pythona %f' % 3)  # Używamy wersji pythona 3.000000
+print('Używamy wersji pythona %.2f' % 3)  # Używamy wersji pythona 3.00
+print('Używamy wersji pythona %.1f' % 3.9)  # Używamy wersji pythona 3.9
+print('Używamy wersji pythona %.0f' % 3.9)  # Używamy wersji pythona 4 - zaokrągli
+print('Używamy wersji pythona %.f' % 3.9)  # Używamy wersji pythona 4 - zaokrągli
+# zaokrągla do wyświetlania
+
+x = 3.8769
+print(x)
+print(f"{x:.2f}")  # 3.88 str
+print(x)  # 3.8769 - liczba się nie zmieni
+y = round(x)
+print(y)  # 4 int, <class 'float'>
+z = round(x, 2)
+print(f"{z=}")  # z=3.88, z pozostaje <class 'float'>
+print(type(z))  # <class 'float'>
+# ctrl alt l - formatowanie wg PEP8
+
+
