1fe2e826ef845615ef6b22642d0f25dd4271a934
rajkonkret <rajkonkret660@gmail.com>
2025-09-22 15:03:37 +0200

    [1 21] update typy_danych_1.py

--- day1/typy_danych_1.py
+++ day1/typy_danych_1.py
@@ -79,3 +79,13 @@ print(True and True)  ##           True
 print(True and False)  ##          False
 print(False and True)  ##          False
 print(False and False)  ##         False
+
+# or - lub
+print(True or True)  ##           True
+print(True or False)  ##          True
+print(False or True)  ##          True
+print(False or False)  ##         False
+
+# not - negaacja
+print(not True)  # False
+print(not False)  # True
