0f6155d10c530d4f578997742aac8ce640219157
rajkonkret <rajkonkret660@gmail.com>
2025-09-22 11:52:27 +0200

    [1 2] add pierwszy.py

---
+++ day1/pierwszy.py
@@ -0,0 +1,29 @@
+print()  # wypisz/wydrukuj
+# Process finished with exit code 0 nie ma błędów
+# PEP8  - zasadyformatowania kodu w Pythonie
+# https://peps.python.org/pep-0008/
+# ctrl alt l - formatowanie kodu
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+print("Radek") # Radek
+# ctrl d - powielanie linii
+print('Radek') # Radek
+# ctrl / - szybkie komentowanie linii
+# print('Radek")
+#   File "C:\Users\CSComarch\PycharmProjects\PythonPodstawy-22-09-2025\day1\pierwszy.py", line 17
+#     print('Radek")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 17)
+#
+# Process finished with exit code 1
+print("Kolejna komenda")
+
+print("Radek \"Radek\"") # \ - znak ucieczki
+# Radek "Radek"
+
--- main.py
+++ main.py
@@ -16,3 +16,4 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # 2 x shift - podręczna wyszukiwarka
 # wheel, theme
+# Hi, PyCharm
