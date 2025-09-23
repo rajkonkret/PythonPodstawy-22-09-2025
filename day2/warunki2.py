ddde47a03f323a46ad40e9e03ce6cdf39d3eedac
rajkonkret <rajkonkret660@gmail.com>
2025-09-23 15:33:35 +0200

    [2 26] add warunki2.py

---
+++ day2/warunki2.py
@@ -0,0 +1,20 @@
+# od python 3.10
+# match case
+
+lista = []
+lang = input("Podaj znany Ci język programowania")
+
+match lang.strip().casefold():
+    case "python":
+        lista.append('Znam Pythona')
+    case 'java':
+        lista.append("Znam Jave")
+    case _:  # odpowiednik else
+        print("Nie znam takiego języka")
+
+print(lista)
+# Podaj znany Ci język programowaniajava
+# ['Znam Jave']
+# Podaj znany Ci język programowaniac
+# Nie znam takiego języka
+# []
