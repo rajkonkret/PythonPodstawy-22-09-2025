cbe4f9f06d61942d6d6dc32c0050342d33bbecfc
rajkonkret <rajkonkret660@gmail.com>
2025-09-23 14:09:53 +0200

    [2 19] add warunki.py

---
+++ day2/warunki.py
@@ -0,0 +1,21 @@
+# instrukcje warunkowe
+# instrukcje sterowania przepływem programu
+# if
+# w zależności od warunku wykona jeden lub drugi blok programu
+# warunek musi zwracać typ bool (prawda, fałsz) True, False
+
+odp = True
+print(bool(True))  # True
+odp= False
+# debugger, pułąpka
+if odp:
+    # blok programu wykonywany gdy warunek True
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print('Dalsza częśc programu niezależna od warunku')
+# True
+# Dalsza częśc programu niezależna od warunku
