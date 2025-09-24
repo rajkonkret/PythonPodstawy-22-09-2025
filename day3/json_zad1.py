47a8bc415973eed2893ba04bdfa2b48e11dd5f56
rajkonkret <rajkonkret660@gmail.com>
2025-09-24 15:15:18 +0200

    [3 22] update json_zad1.py

--- day3/json_zad1.py
+++ day3/json_zad1.py
@@ -50,3 +50,19 @@ print(data)
 print(type(data))  # <class 'dict'>
 
 print(data['name'])  # Radek
+
+# zamiana słownika na json
+json_text = json.dumps(data)
+print(json_text)  # {"age": 40, "czy_pali": null, "name": "Radek"}
+print(type(json_text))  # <class 'str'>
+
+dict_json = json.loads(json_text)
+print(dict_json)  # {'age': 40, 'czy_pali': None, 'name': 'Radek'}
+print(type(dict_json))  # <class 'dict'>
+
+# wypisze klucze
+for k in dict_json:
+    print(k)
+# age
+# czy_pali
+# name
