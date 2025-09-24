7f067ba9f8e6be3e125f72ead00f253bdaaf656c
rajkonkret <rajkonkret660@gmail.com>
2025-09-24 12:10:16 +0200

    [3 11] update petle_zad3.py

---
+++ day3/kalkulator.py
@@ -0,0 +1,7 @@
+# napisać program kalkulator z wykorzystaniem pętli while True
+# menu z dostępnymi opcjami
+# ma mieć opcję wyjscia
+# + - * /
+# wyświetlić wynik w sposób sformatowany
+# obsłużyć wyjątki
+
