2df7d5f99f109b5c2400a0150ab418952c85049f
rajkonkret <rajkonkret660@gmail.com>
2025-09-24 09:44:16 +0200

    [3 2] update random_zad1.py

---
+++ day3/petle_zad1.py
@@ -0,0 +1,18 @@
+# pętla - pozwala wielokrotnie wykonać fragment kodu
+# for - pętla iteracyjna
+
+for i in range(5):  # od 0 do 4
+    print(i)
+
+for i in range(10):
+    pass  # nic nie rób
+
+for _ in range(5):  # niema zmienna
+    print("Test podłoga")
+    print(_)
+
+for i in range(5):
+    print(i + 2)
+    print(i * 2)
+
+#  przerobić na pętle lotto
