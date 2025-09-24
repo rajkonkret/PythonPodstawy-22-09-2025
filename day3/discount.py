36f5e63c3ceb59f951752098d8bc401ac6662a13
rajkonkret <rajkonkret660@gmail.com>
2025-09-24 13:48:05 +0200

    [3 14] add discount.py

---
+++ day3/discount.py
@@ -0,0 +1,30 @@
+from datetime import date, datetime, timedelta
+
+today = date.today()
+print("Dzisiejsza data:", today)  # Dzisiejsza data: 2025-09-24
+print(type(today))  # <class 'datetime.date'>
+
+time = datetime.now()
+print('Aktualna godzina:', time)  # Aktualna godzina: 2025-09-24 13:36:05.345565
+print(type(time))  # <class 'datetime.datetime'>
+
+print(today.year)
+print(today.day)
+# 2025
+# 24
+
+print(time.hour)
+print(time.minute)
+# 13
+# 37
+
+formated_date = datetime.now().strftime("%d/%m/%Y")
+print("Sformatowana data:", formated_date)  # Sformatowana data: 24/09/2025
+
+# 13:39
+formated_time = datetime.now().strftime("%H:%M:%S")
+print("Sforamtowana godzina:", formated_time)  # Sforamtowana godzina: 13:42:52
+
+# 12h
+formated_time_12h = datetime.now().strftime("%I:%M:%S %p")
+print("Sforamtowana godzina (12h):", formated_time_12h)  # Sforamtowana godzina (12h): 01:46:47 PM
