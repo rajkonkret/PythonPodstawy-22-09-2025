25851ba1e68bd061cd2d5684c82aa105e29f2108
rajkonkret <rajkonkret660@gmail.com>
2025-09-25 12:48:49 +0200

    [4 11] update fun9.py

--- day4/fun9.py
+++ day4/fun9.py
@@ -27,3 +27,27 @@ print(wiek(10))  # nastolatek
 print(wiek(17))  # nastolatek
 print(wiek(18))  # dorosły
 print(wiek(25))  # dorosły
+
+lista = [1, 2, 14, 24, 50, 67, 80, 100, 200, 500]
+l1 = []
+for i in lista:
+    l1.append(i * 2)
+print(l1)  # [2, 4, 28, 48, 100, 134, 160, 200, 400, 1000]
+
+print([i * 2 for i in lista])  # [2, 4, 28, 48, 100, 134, 160, 200, 400, 1000]
+
+
+def zmien(x):
+    return i * 2
+
+
+l2 = []
+for i in lista:
+    l2.append(zmien(i))
+print(l2)  # [2, 4, 28, 48, 100, 134, 160, 200, 400, 1000]
+
+zmien2 = lambda x: x * 2
+l3 = []
+for i in lista:
+    l3.append(zmien2(i))
+print(l3)  # [2, 4, 28, 48, 100, 134, 160, 200, 400, 1000]
