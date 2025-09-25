4955d649409996eb27a6a07925de9da1813f02e0
rajkonkret <rajkonkret660@gmail.com>
2025-09-25 14:57:08 +0200

    [4 19] add kl1.py

---
+++ day4/kl1.py
@@ -0,0 +1,33 @@
+# klasa - element programowania obiektowego
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcje w klasie
+# obiekt - instancja klasy
+# kalsa musi zostać zadeklarowana przed użyciem
+# tworzenie oboektu klasy uruchamia funkcję inicjalizującą (konstruktor) __init__
+# paradygmaty -> hermetyzacja, dziedziczenie,  polimorfizm, abstrakcja
+
+# PascalCase, UpperCamelCase
+class Human:
+    """
+    Klasa Human opisująca człowieka w pythonie
+    """
+
+
+print(Human.__doc__)  # Klasa Human opisująca człowieka w pythonie
+print(print.__doc__)
+# Prints the values to a stream, or to sys.stdout by default.
+#
+#   sep
+#     string inserted between values, default a space.
+#   end
+#     string appended after the last value, default a newline.
+#   file
+#     a file-like object (stream); defaults to the current sys.stdout.
+#   flush
+#     whether to forcibly flush the stream.
+
+#  cd .. - wyjscie z katalogu
+#  cd day4 - wejście do katalogu day4
+#  pydoc -b - serwer dokumentacji
+#  pydoc -w kl1 - generuje plik kl1.html z dokumentacja
