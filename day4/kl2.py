5eb9de3a6dd703ec031a08cfd618bea2e8e5f1ed
rajkonkret <rajkonkret660@gmail.com>
2025-09-25 15:15:23 +0200

    [4 21] add kl2.py

---
+++ day4/kl2.py
@@ -0,0 +1,17 @@
+class Human:
+    """
+    Klasa Human opisująca człowieka w pythonie
+    """
+
+    def __init__(self, imie, wiek, wzrost, plec="k"):
+        """
+        Metoda inicjalizująca - konstruktor
+        :param imie:
+        :param wiek:
+        :param wzrost:
+        :param plec:
+        """
+        self.imie = imie
+        self.wiek = wiek
+        self.wzrost = wzrost
+        self.plec = plec
