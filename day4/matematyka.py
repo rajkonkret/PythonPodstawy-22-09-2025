720b3ce639cca393dbbda02e8d54445bb6ed259e
rajkonkret <rajkonkret660@gmail.com>
2025-09-25 14:14:37 +0200

    [4 17] update matematyka.py

--- day4/matematyka.py
+++ day4/matematyka.py
@@ -1,4 +1,5 @@
 import math
+from itertools import zip_longest
 
 print(math.pi)  # 3.141592653589793
 print(math.sqrt(25))  # 5.0 -> float
@@ -22,3 +23,36 @@ for a, s in zip(angles, sin_values):
 
 imiona = ["Radek", "Tomek", "Agata", "Marek", "Anna"]
 wiek = [44, 56, 23, 34]
+
+zipped = zip_longest(imiona, wiek, fillvalue=None)
+print(zipped)  # <itertools.zip_longest object at 0x000002C2C685B380> iterator
+
+# for item in zipped:
+#     print(item)
+# ('Radek', 44)
+# ('Tomek', 56)
+# ('Agata', 23)
+# ('Marek', 34)
+# ('Anna', None)
+
+print(40 * "-")
+
+# tylko raz możemy odczytać dane z iteratora
+# for o, w in zipped:
+#     print(o, w)
+# ----------------------------------------
+# Radek 44
+# Tomek 56
+# Agata 23
+# Marek 34
+# Anna None
+
+lista_zipped = list(zipped)
+print(lista_zipped)
+for o, w in lista_zipped:
+    print(o, w)
+# Radek 44
+# Tomek 56
+# Agata 23
+# Marek 34
+# Anna None
