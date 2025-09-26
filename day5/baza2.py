a6663b2629e283b2528065468fe76bcfcc523786
rajkonkret <rajkonkret660@gmail.com>
2025-09-26 14:32:04 +0200

    [5 21] update baza2.py

--- day5/baza2.py
+++ day5/baza2.py
@@ -10,18 +10,64 @@ try:
     print("Baaza danych zostałą podłączona")
 
     query = """
-    CREATE TABLE IF NOT EXISTS developers(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    salary REAL NOT NULL);
+            CREATE TABLE IF NOT EXISTS developers
+            (
+                id
+                INTEGER
+                PRIMARY
+                KEY,
+                name
+                TEXT
+                NOT
+                NULL,
+                email
+                TEXT
+                NOT
+                NULL
+                UNIQUE,
+                salary
+                REAL
+                NOT
+                NULL
+            );
             """
 
     c.execute(query)
     conn.commit()
+
+    insert = """
+             INSERT INTO developers (id, name, email, salary)
+             VALUES (1, 'Radek', 'raj@raj.pl', 10000);
+             """
+
+    # c.execute(insert)
+    # conn.commit()
+
+    select = "SELECT * FROM developers;"
+    for row in c.execute(select):
+        print(row)
+        # (1, 'Radek', 'raj@raj.pl', 10000.0)
+
+    update = """
+             UPDATE developers
+             SET salary=20000
+             WHERE id = 1;"""
+
+    c.execute(update)
+    conn.commit()  # (1, 'Radek', 'raj@raj.pl', 20000.0)
+
+    delete = """
+             DELETE
+             FROM developers
+             WHERE id = 1;
+             """
+    c.execute(delete)
+    conn.commit()
+
 except sqlite3.Error as e:
     print("Bład połaczenia z bazą:", e)
 finally:
     if conn:
         conn.close()
         print('Połączenie zostało zamknięte')
+# CRUD
