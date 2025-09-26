c7d1151cd6d345498cf7cbc6e0f04fc0b2051665
rajkonkret <rajkonkret660@gmail.com>
2025-09-26 12:36:59 +0200

    [5 14] add api_chuck_norris.py

---
+++ day5/api_chuck_norris.py
@@ -0,0 +1,13 @@
+# REST API (Reprezentational State Transfer Application Programming Interface) to interfejs programowania aplikacji,
+# który przestrzega zasad stylu architektonicznego REST,
+# umożliwiając aplikacjom komunikację i wymianę danych.
+# Korzysta ze standardowych protokołów HTTP (takich jak GET, POST, PUT, DELETE)
+# Metody HTTP: Do operacji na zasobach wykorzystuje się standardowe metody HTTP:
+# GET: Pobranie zasobu.
+# POST: Utworzenie nowego zasobu.
+# PUT: Zaktualizowanie istniejącego zasobu.
+# DELETE: Usunięcie zasobu
+# https://api.chucknorris.io/
+
+
+url = "https://api.chucknorris.io/"
